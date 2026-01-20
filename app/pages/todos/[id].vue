c84f1234090bb0dff2eaf19b15d5bfefa09f0984
Jonelskie <jonel.uligan7@gmail.com>
2026-01-20 23:56:14 +0800

    Implement Page Transitions.

--- app/pages/todos/[id].vue
+++ app/pages/todos/[id].vue
@@ -10,8 +10,7 @@
 
 		<NuxtLink class="btn" :to="`/todos/edit/${todo?.id}`">Edit</NuxtLink>
 
-		<button 
-			@click="todoStore.updateMarkCompletedStatus(todoId)" 
+		<button @click="todoStore.updateMarkCompletedStatus(todoId)"
 			:class="todo?.isCompleted ? 'btn-danger' : 'btn-success'">
 			Mark As {{ todo?.isCompleted ? 'Not Completed' : 'Completed' }}
 		</button>
@@ -20,72 +19,78 @@
 </template>
 
 <script setup lang="ts">
-	useSeoMeta({
-		title: 'Nuxt - Todo Details',
-		description: 'Nuxt Todo App - Details Page'
-	});
+definePageMeta({
+	pageTransition: {
+		name: 'rotate'
+	}
+})
 
-	const route = useRoute();
+useSeoMeta({
+	title: 'Nuxt - Todo Details',
+	description: 'Nuxt Todo App - Details Page'
+});
 
-	const todoStore = useTodoStore();
-	const { todos, getTodoById } = storeToRefs(todoStore);
+const route = useRoute();
 
-	const todoId  = Number(route?.params.id);
+const todoStore = useTodoStore();
+const { todos, getTodoById } = storeToRefs(todoStore);
 
-	const todo = computed(() => {
-		return getTodoById.value(todoId);
-	});
+const todoId = Number(route?.params.id);
+
+const todo = computed(() => {
+	return getTodoById.value(todoId);
+});
 
 </script>
 
 <style lang="css">
-	.btn {
-		background-color: #007bff;
-		color: white;
-		padding: 10px 20px;
-		border: none;
-		border-radius: 5px;
-		cursor: pointer;
-		border-radius: 4px;
-		text-decoration: none;
-		margin: 10px;
-		font-size: 16px;
-	}
+.btn {
+	background-color: #007bff;
+	color: white;
+	padding: 10px 20px;
+	border: none;
+	border-radius: 5px;
+	cursor: pointer;
+	border-radius: 4px;
+	text-decoration: none;
+	margin: 10px;
+	font-size: 16px;
+}
 
-	.btn:hover {
-		background-color: #0056b3;
-		color: white;
-	}
+.btn:hover {
+	background-color: #0056b3;
+	color: white;
+}
 
-	.btn-success {
-		background-color: #28a745;
-		color: white;
-		border: none;
-		padding: 10px 20px;
-		cursor: pointer;
-		border-radius: 4px;
-		text-decoration: none;
-		margin: 10px;
-		font-size: 16px;
-	}
+.btn-success {
+	background-color: #28a745;
+	color: white;
+	border: none;
+	padding: 10px 20px;
+	cursor: pointer;
+	border-radius: 4px;
+	text-decoration: none;
+	margin: 10px;
+	font-size: 16px;
+}
 
-	.btn-success:hover {
-		background-color: #218838;
-	}
+.btn-success:hover {
+	background-color: #218838;
+}
 
-	.btn-danger {
-		background-color: #dc3545;
-		color: white;
-		border: none;
-		padding: 10px 20px;
-		cursor: pointer;
-		border-radius: 4px;
-		text-decoration: none;
-		margin: 10px;
-		font-size: 16px;
-	}
+.btn-danger {
+	background-color: #dc3545;
+	color: white;
+	border: none;
+	padding: 10px 20px;
+	cursor: pointer;
+	border-radius: 4px;
+	text-decoration: none;
+	margin: 10px;
+	font-size: 16px;
+}
 
-	.btn-danger:hover {
-		background-color: #c82333;
-	}
+.btn-danger:hover {
+	background-color: #c82333;
+}
 </style>
