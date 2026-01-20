60ef21c4981ef5246c3d8068f85bf7a9e87f5f0e
Jonelskie <jonel.uligan7@gmail.com>
2026-01-21 03:44:23 +0800

    Implement Handling Errors and Creating Custom Error Pages.

---
+++ app/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Error ! : {{ error }}</h2>
+        <button @click="handleError">Clear</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const props = defineProps({
+    error: Object as () => NuxtError
+});
+
+const handleError = () => clearError({ redirect: '/' });
+
+</script>
